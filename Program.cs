bc1619e98e8620e56d4d76864777c42cc892d792
Karina Gwalani <128471243+Karina-Abjima@users.noreply.github.com>
2023-04-06 18:46:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Test
+{
+    public record Person(string FirstName, string LastName)//record
+    {
+        public string[] PhoneNumbers { get; init; }//string array
+    }
+    class A
+    {
+        public static void Main(String[] args)
+        {
+            Person person1 = new("Nancy", "Davolio") { PhoneNumbers = new string[1] };
+            Console.WriteLine(person1);
+          
+            Person person2 = person1 with { FirstName = "John" };
+            Console.WriteLine(person2);
+           
+            Console.WriteLine(person1 == person2); // output: False
+
+            person2 = person1 with { PhoneNumbers = new string[1] };
+            Console.WriteLine(person2);
+           
+            Console.WriteLine(person1 == person2); // output: False
+
+            person2 = person1 with { };
+            Console.WriteLine(person1 == person2); // output: True
+            int? a = null;
+            int b = a ?? 6;
+            Console.WriteLine(b);
+            Console.WriteLine(a);//null
+            string str = "Hello";
+            int len = str.Length;
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = i; j < len; j++)
+                {
+                    try
+                    {
+
+
+                        Console.Write(str.Substring(i, (j - i) + 1) + ",");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
